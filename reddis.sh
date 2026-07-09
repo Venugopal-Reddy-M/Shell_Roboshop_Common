7f664392710335392d4ff73210345658dfc6f6f1
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-09 20:09:54 +0530

    actions

--- reddis.sh
+++ reddis.sh
@@ -6,29 +6,29 @@ app_name=reddis
 check_root
 
 if [ $? -ne 0 ]; then
-   dnf module disable redis -y
+   dnf module disable redis -y &>>$LOGS_FILE
    VALIDATE $? "Disable redis..."
 else 
  echo "disable redis already"
 fi
 
 if [ $? -ne 0 ]; then
-  dnf module enable redis:7 -y
+  dnf module enable redis:7 -y &>>$LOGS_FILE
   VALIDATE $? "Enable redis..."
 else
   echo "Enable redis...alredy"
 fi  
 
 dnf install redis -y 
-VALIDATE $? "Installing redis..." 
+VALIDATE $? "Installing redis..." &>>$LOGS_FILE
 
 #mkdir -p $CONFIC_REDIS
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOGS_FILE
 VALIDATE $? "Allowing remote..."
 
-systemctl enable redis 
-systemctl start redis 
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis &>>$LOGS_FILE
 VALIDATE $? "enable and start.... "
 
 print_total_time 
